e6fd18dad0a0494bd39ce06bfe8bd22ed96f1c97
신중하 <sjh146@naver.com>
2025-02-09 22:36:30 +0900

    'dart인증키발급'

--- DartApi.py
+++ DartApi.py
@@ -1,9 +1,9 @@
 #dartApiKey='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
-import requests
-from io import BytesIO
-import zipfile
-import xmltodict
 
-api = 'https://opendart.fss.or.kr/api/corpCode.xml'
-res = requests.get(api, params={'crtfc_key': crtfc_key})
-data_xml = zipfile.ZipFile(BytesIO(res.content))
+import dart_fss as dart_fss
+import pandas as pd 
+
+api_key='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
+
+
+
