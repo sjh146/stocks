8abd9aad7fc6dd7262cf2b89b75c9f658b343289
신중하 <sjh146@naver.com>
2025-02-09 23:02:07 +0900

    '수정'

--- DartApi.py
+++ DartApi.py
@@ -7,5 +7,3 @@ api_key='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
 
 dart.set_api_key(api_key=api_key)
 
-corp_list = dart.get_corp_list()
-samsung = corp_list.find_by_corp_name('삼성전자', exactly=True)[0]
