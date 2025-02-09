93c4649ae29166ec8015124e09c34c8830e5b9f1
신중하 <sjh146@naver.com>
2025-02-10 00:48:30 +0900

    'request사용'

--- DartApi.py
+++ DartApi.py
@@ -1,9 +1,31 @@
 #dartApiKey='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
 #url="https://opendart.fss.or.kr/api/corpCode.xml"
-import dart_fss as dart
 
+import requests
+import io
+import zipfile
+import xmltodict
+import pandas as pd
+
+url="https://opendart.fss.or.kr/api/corpCode.xml"
 
 api_key='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
 
 
-dart.set_api_key(api_key=api_key)
+params = {
+    "crtfc_key": key
+}
+resp = requests.get(url, params=params)
+
+f = io.BytesIO(resp.content)
+zfile = zipfile.ZipFile(f)
+
+zfile.namelist()
+
+xml = zfile.read("CORPCODE.xml").decode("utf-8")
+dict_data = xmltodict.parse(xml)
+
+data = dict_data['result']['list']
+df = pd.DataFrame(data)
+
+df.head()
