0908fee578f2e9a000611ec17b1d03d55b3ba6be
신중하 <sjh146@naver.com>
2025-02-09 22:48:53 +0900

    '오류수정

--- DartApi.py
+++ DartApi.py
@@ -1,9 +1,11 @@
 #dartApiKey='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
 
-import dart_fss as dart_fss
-import pandas as pd 
+import dart_fss as dart
+
 
 api_key='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
 
+dart.set_api_key(api_key=api_key)
 
-
+corp_list = dart.get_corp_list()
+samsung = corp_list.find_by_corp_name('삼성전자', exactly=True)[0]
