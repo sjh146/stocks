d8f6b39796d0f06a3f899e270cb6011d6c155fa9
신중하 <sjh146@naver.com>
2025-02-09 23:25:33 +0900

    수정중

--- DartApi.py
+++ DartApi.py
@@ -1,9 +1,9 @@
 #dartApiKey='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
-
+#url="https://opendart.fss.or.kr/api/corpCode.xml"
 import dart_fss as dart
 
 
 api_key='a7910deaf71437b7ca18e2184ea4c9696eb82ecb'
 
-dart.set_api_key(api_key=api_key)
 
+dart.set_api_key(api_key=api_key)
