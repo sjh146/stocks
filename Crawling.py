178fc2c5f50d093c04545f971f70a6aaf18292aa
신중하 <sjh146@naver.com>
2025-02-13 21:41:07 +0900

    '수정'

--- Crawling.py
+++ Crawling.py
@@ -8,29 +8,18 @@ class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+   
+
+
+
 
     def initUI(self):
-        mp = QCheckBox('시가', self)
-        mp.move(20, 20)
+           
+        mp = QCheckBox('거래량', self)
+        mp.move(30, 50)
         mp.stateChanged.connect(self.changeTitle)
 
-        tv = QCheckBox('거래량', self)
-        tv.move(20, 20)
-        tv.stateChanged.connect(self.changeTitle)
-
-        cl = QCheckBox('고가', self)
-        cl.move(20, 20)
-        cl.stateChanged.connect(self.changeTitle)
-
-        lp = QCheckBox('저가', self)
-        lp.move(20, 20)
-        lp.stateChanged.connect(self.changeTitle)
-
-        bp = QCheckBox('매수호가', self)
-        bp.move(20, 20)
-        bp.stateChanged.connect(self.changeTitle)
-
-
+            
 
 
 
