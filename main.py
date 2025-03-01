62547db9f6893610293b7a0a5a6752663e93231a
신중하 <sjh146@naver.com>
2025-03-01 21:42:20 +0900

    '블로그작성수정'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import ollama
+
 import eel
 import sys
 import pyperclip
@@ -10,8 +10,9 @@ import sys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from user_agent import generate_user_agent, generate_navigator
-
+from selenium.webdriver.common.action_chains import ActionChains
+import ollama
+from fake_useragent import UserAgent
 eel.init('web')
 @eel.expose
 def insert_blog(uun, uid, upw):
@@ -48,18 +49,17 @@ def resource_path(relative_path):
  
 @eel.expose
 def crawl(uid,upw):
-    navigator = generate_navigator()
-    print(navigator)
-    print(navigator['platform'])
-    chromedriver_path =resource_path("chromedriver.exe")
-    header=generate_user_agent(os='win', device_type='desktop')       
-    #header='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
+    #ua=UserAgent()
+   # header =ua.chrome
+    #print(header)
+    chromedriver_path =resource_path("chromedriver.exe")     
+    header='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
     options = webdriver.ChromeOptions()
     #options.add_argument('--headless')
     #options.add_argument('--disable-gpu')  # Optional: run in headless mode
-    options.add_argument('user-agent=' + header)
+    options.add_argument('user-agent=:' + header)
     options.add_argument("disable-blink-features=AutomationControlled")
-
+   # options.add_argument(r'user-data-dir=C:\Users\Admin\AppData\Local\Google\Chrome\User Data\Default')
     options.add_experimental_option("detach",True)
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
@@ -96,37 +96,45 @@ def crawl(uid,upw):
     login_btn.click()
     time.sleep(2)
     
-    #Find, click adn type in the search_box
-    search_box =driver.find_element(By.CSS_SELECTOR,'#query')
-    search_box.click()
-    search_box.send_keys('네이버블로그')
-    search_btn =driver.find_element(By.CSS_SELECTOR,'#search-btn')
-    search_btn.click()
+    driver.get('https://blog.naver.com/{}?Redirect=Write&'.format(uid))
+    time.sleep(5)
+    try:
+        iframe_list = driver.find_elements('css selector', 'iframe')
+        #iframe = iframe_list[0]
+        print(iframe_list)
+        #iframe_name = iframe.get_attribute('name')
+        #driver.switch_to.frame(iframe_name)
+        #driver.find_elements('css selector', '종료 버튼 css selector')[0].click()
+        driver.switch_to.window(driver.window_handles[0]) 
+
+    except:
+        print('임시저장창없음')
+
     time.sleep(2)
+    driver.switch_to.frame('mainFrame')
+    time.sleep(4)
+  
+    actions=ActionChains(driver)
+    title=driver.find_element(By.XPATH,'//span[contains(text(),"제목")]')
+    actions.click(title).perform()
     
-    #Find and click naverblog
-    driver.execute_script("window.scrollTo(0, 500)")
-    nblog=driver.find_element(By.CSS_SELECTOR,'#main_pack > section.sc_new.sp_nsite._project_channel_site_root._fe_site_collection._prs_vsd_bas > div > div > div.nsite_tit > div > div.nsite_name > a > mark')
-    nblog.click()
+    actions.send_keys('블로그제목입니다.').perform()
+    
+    article=driver.find_element(By.XPATH,'//span[contains(text(),"본문에 #을 이용하여 태그를 사용해보세요! (최대 30개)")]')
+    actions.click(article).perform()
+    
+    #blog_title=ollama.chat(model='llama3',messages=[
+        #{
+            #'role':'user',
+            #'content':'give me one simple blog title',
+
+        #},
+    
+    #])
+    
     time.sleep(2)
+    #actions.send_keys(blog_title['message']['content']).perform()
+
     
-    #Find and click write_btn
-    driver.switch_to.window(driver.window_handles[1])
-    write_btn=driver.find_element(By.CSS_SELECTOR,'#container > div > aside > div > div:nth-child(1) > nav > a:nth-child(2)')
-    write_btn.click()
-    time.sleep(1)
-
-    blog_title=ollama.chat(model='llama3',messages=[
-        {
-            'role':'user',
-            'content':'your good assistant',
-
-        },
-    
-    ])
-    blog_title['message']['content']
-
-    with sqlite3.connect('blog.db') as conn:
-        conn.close()
+       
 eel.start('index.html', size=(600, 400))
- #SE-99c10990-f8cf-4208-9ea2-0bda71528e48 > div.se-wrap.se-dnd-wrap > div > div.se-popup.__se-sentry.se-popup-alert.se-popup-alert-confirm > div.se-popup-container.__se-pop-layer > div.se-popup-button-container > button.se-popup-button.se-popup-button-confirm > span
