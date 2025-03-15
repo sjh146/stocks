36dcf180d2b1ea3051f191c6d4190edfd34403e9
신중하 <sjh146@naver.com>
2025-03-15 22:15:50 +0900

    '로그인창띄우기'

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-from urllib.parse import urljoin
-from urllib.robotparser import RobotFileParser
+from pykiwoom.kiwoom import *
 
-url="https://wwww.inflearn.com/course/실습-파이썬-웹크롤링-웹페이지-자동화"
-robot_url=urljoin(url,"/robots.txt")
-print(robot_url)
-robot_parser=RobotFileParser()
-robot_parser.set_url(robot_url)
-robot_parser.read()
-print (robot_parser)
-print()
-if robot_parser.can_fetch("Mybot",url):
-    print("Allow")
-else:
-    print("Disallow")
+kiwoom = Kiwoom()
+kiwoom.CommConnect(block=True)
+print("블록킹 로그인 완료")
